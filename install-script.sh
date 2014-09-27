6e4bbc81d8e18adca21f77d64715d7c9c43f0039
jamesbeedy <jamesbeedy@gmail.com>
2014-09-27 09:01:16 -0700

    added user input args for virtualenv and project name to install script

--- install-script.sh
+++ install-script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt-get install python-pip curl
 sudo pip install virtualenv
-virtualenv ~/.virtualenvs/testjs
-. ~/.virtualenvs/testjs/bin/activate
+virtualenv ~/.virtualenvs/"$1"
+. ~/.virtualenvs/"$1"/bin/activate
 pip install -e git+https://github.com/ekalinin/nodeenv.git#egg=nodeenv
 nodeenv -p
 npm install -g bower
